64daa90784dd8513d1bf7796e7034286bd20595b
MateoCorDuarte <cortesdumateo@hotmail.com>
2025-08-22 15:49:50 +0200

    Stack widget same window

--- Happy_walking_Tour_UI/mainwindow.cpp
+++ Happy_walking_Tour_UI/mainwindow.cpp
@@ -15,7 +15,11 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_create_new_tour_clicked()
 {
+    create_new_tour_window = new Create_new_tour_window(this);
 
+    create_new_tour_window->show();
+
+    this->hide();
 }
 
 
